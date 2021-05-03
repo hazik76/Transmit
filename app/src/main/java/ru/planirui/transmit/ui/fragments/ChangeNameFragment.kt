401c40f9f19dd551394ad071e220b2ae32626d05
Alexey Khazov <ahazov@haz.ru>
2021-05-03 19:57:40 +0300

    Устранение бага Имя/Фамлия

--- app/src/main/java/ru/planirui/transmit/ui/fragments/ChangeNameFragment.kt
+++ app/src/main/java/ru/planirui/transmit/ui/fragments/ChangeNameFragment.kt
@@ -9,24 +9,27 @@ class ChangeNameFragment : BaseFragment(R.layout.fragment_change_name) {
 
     override fun onResume() {
         super.onResume()
-        (activity as MainActivity).title = getString(R.string.action_my_account) //не меняется по сравнению с предыдущим фрагментом
+        (activity as MainActivity).title =
+            getString(R.string.action_my_account) //не меняется по сравнению с предыдущим фрагментом
         //setHasOptionsMenu(true)
         val fullnameList = USER.fullname.split(" ")
-        settings_input_name.setText(fullnameList[0])
-        settings_input_surname.setText(fullnameList[1])
-        register_btn_next.setOnClickListener({ changeName() })
+        if (fullnameList.size > 1) {
+            settings_input_name.setText(fullnameList[0])
+            settings_input_surname.setText(fullnameList[1])
+        } else settings_input_name.setText(fullnameList[0])
+        register_btn_next.setOnClickListener { changeName() }
     }
 
     private fun changeName() {
         val name = settings_input_name.text.toString()
         val surname = settings_input_surname.text.toString()
-        if (name.isEmpty()){
+        if (name.isEmpty()) {
             showToast(getString(R.string.settings_toast_name_is_empty))
         } else {
             val fullname = "$name $surname"
             REF_DATABASE_ROOT.child(NODE_USERS).child(UID).child(CHILD_FULLNAME)
                 .setValue(fullname).addOnCompleteListener {
-                    if (it.isSuccessful){
+                    if (it.isSuccessful) {
                         showToast(getString(R.string.toast_data_update))
                         USER.fullname = fullname
                         fragmentManager?.popBackStack()

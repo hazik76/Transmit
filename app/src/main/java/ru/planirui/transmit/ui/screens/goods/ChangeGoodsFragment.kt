9adf7bf3606e9248270d9173a2682c394c9cf1d6
hazik76 <alex@haz.ru>
2021-05-18 18:41:19 +0300

    GhangeGoodsFragment попытка перевода к Binding

--- app/src/main/java/ru/planirui/transmit/ui/screens/goods/ChangeGoodsFragment.kt
+++ app/src/main/java/ru/planirui/transmit/ui/screens/goods/ChangeGoodsFragment.kt
@@ -1,8 +1,8 @@
 package ru.planirui.transmit.ui.screens.goods
 
-import kotlinx.android.synthetic.main.fragment_change_bio.*
 import ru.planirui.transmit.R
 import ru.planirui.transmit.database.*
+import ru.planirui.transmit.databinding.FragmentChangeBioBinding
 import ru.planirui.transmit.ui.screens.base.BaseChangeFragment
 
 /* Фрагмент для изменения описания товара, если товара не было, то с добавлением всех недостающих полей */
@@ -13,13 +13,16 @@ class ChangeGoodsFragment(
     private val changeName: String
 ) : BaseChangeFragment(R.layout.fragment_change_bio) {
 
+    private lateinit var binding: FragmentChangeBioBinding
+
     override fun onResume() {
         super.onResume()
         initFields()
     }
 
     private fun initFields() {
-        settings_input_bio.setText(description)
+        binding = FragmentChangeBioBinding.inflate(layoutInflater)
+        binding.settingsInputBio.setText(description)
     }
 
     override fun change() {
@@ -34,7 +37,7 @@ class ChangeGoodsFragment(
     }
 
     private fun change2() {
-        val newDescriptions = settings_input_bio.text.toString()
+        val newDescriptions = binding.settingsInputBio.text.toString()
         when(changeName){
             "description" -> setGoodsDescriptionsToDatabase(newDescriptions, idGoods)
             "extend" ->  setGoodsExtendToDatabase(newDescriptions, idGoods)
